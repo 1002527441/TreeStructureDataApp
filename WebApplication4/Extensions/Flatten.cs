6d855fb7f7bb2e7fb121039d823c5ffff2564d99
Henry.zhang <1002527441@qq.com>
2024-02-02 12:21:16 +0800

    Update Flatten.cs

--- WebApplication4/Extensions/Flatten.cs
+++ WebApplication4/Extensions/Flatten.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using Microsoft.Identity.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,22 +10,109 @@ namespace WebApplication4.Extensions
     public static class Linq
     {
 
-        public static IEnumerable<T> Flatten1<T>(this T source, Func<T, IEnumerable<T>> selector)
+
+
+        /// <summary>
+        ///   This method extends the LINQ methods to flatten a collection of 
+        ///   items that have a property of children of the same type.
+        /// </summary>
+        /// <typeparam name = "T">Item type.</typeparam>
+        /// <param name = "source">Source collection.</param>
+        /// <param name = "childPropertySelector">
+        ///   Child property selector delegate of each item. 
+        ///   IEnumerable'T' childPropertySelector(T itemBeingFlattened)
+        /// </param>
+        /// <returns>Returns a one level list of elements of type T.</returns>
+        public static IEnumerable<T> Flatten<T>(this IEnumerable<T> source, Func<T, IEnumerable<T>> childPropertySelector)
         {
-            return selector(source).SelectMany(c => Flatten1(c, selector)).Concat(new[] { source });
+            return source
+                .Flatten((itemBeingFlattened, objectsBeingFlattened) =>
+                         childPropertySelector(itemBeingFlattened));
         }
 
-        public static IEnumerable<T> Flatten2<T>(this T source, Func<T, IEnumerable<T>> selector)
+        /// <summary>
+        ///   This method extends the LINQ methods to flatten a collection of 
+        ///   items that have a property of children of the same type.
+        /// </summary>
+        /// <typeparam name = "T">Item type.</typeparam>
+        /// <param name = "source">Source collection.</param>
+        /// <param name = "childPropertySelector">
+        ///   Child property selector delegate of each item. 
+        ///   IEnumerable'T' childPropertySelector
+        ///   (T itemBeingFlattened, IEnumerable'T' objectsBeingFlattened)
+        /// </param>
+        /// <returns>Returns a one level list of elements of type T.</returns>
+        public static IEnumerable<T> Flatten<T>(this IEnumerable<T> source,
+            Func<T, IEnumerable<T>, IEnumerable<T>> childPropertySelector)
         {
-            var stack = new Stack<T>();
-            stack.Push(source);
-            while (stack.Count > 0)
+            return source
+                .Concat(source
+                            .Where(item => childPropertySelector(item, source) != null)
+                            .SelectMany(itemBeingFlattened =>
+                                        childPropertySelector(itemBeingFlattened, source)
+                                            .Flatten(childPropertySelector)));
+        }
+
+
+
+        public static IEnumerable<T> Flatten1<T>(this IEnumerable<T> source, Func<T, IEnumerable<T>> childrenselector)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+
+            if (childrenselector == null)
+                throw new ArgumentNullException(nameof(childrenselector));
+
+            return flatteniterator(source, childrenselector);
+        }
+
+
+
+        private static IEnumerable<T> flatteniterator<T>(this IEnumerable<T> source,Func<T, IEnumerable<T>> childrenselector)
+        {
+            if (source != null)
             {
-                var current = stack.Pop();
-                yield return current;
-                foreach (var child in selector(current))
-                    stack.Push(child);
+                foreach (var item in source)
+                {
+                    yield return item;
+                    var children = childrenselector(item);
+                    if (children != null)
+                        foreach (var child in flatteniterator(children, childrenselector))
+                            yield return child;
+                }
             }
         }
+
+
+
+
+ 
+
+
+        public static IEnumerable<T> Flatten3<T>(this IEnumerable<T> sequence, Func<T, IEnumerable<T>> childFetcher)
+        {
+            var itemsToYield = new Queue<T>(sequence);
+            while (itemsToYield.Count > 0)
+            {
+                var item = itemsToYield.Dequeue();
+                yield return item;
+
+                var children = childFetcher(item);
+                if (children != null)
+                {
+                    foreach (var child in children)
+                    {
+                        itemsToYield.Enqueue(child);
+                    }
+                }
+            }
+        }
+    }
+
+    // 遍历的类型
+    public enum TraverseType
+    {
+        PostOrder,
+        PreOrder
     }
  }
