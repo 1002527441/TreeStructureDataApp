836cd6955b7b7da273e2be2dffe0f0c529258ee6
Henry.zhang <1002527441@qq.com>
2024-02-02 12:24:14 +0800

    Add files via upload

---
+++ WebApplication4/Entities/EmployeeParentDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication4.Entities
+{
+    public class EmployeeParentDTO
+    {
+        public string Id { get; set; }
+        public string Name { get; set; } 
+        public EmployeeParentDTO? Parent { get; set; }
+       
+    }
+}
