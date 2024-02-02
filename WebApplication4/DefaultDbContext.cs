f8e2e1efa0d8dbeeecc9f15790210549a9d4bb18
Henry.zhang <1002527441@qq.com>
2024-02-02 12:22:15 +0800

    Update DefaultDbContext.cs

--- WebApplication4/DefaultDbContext.cs
+++ WebApplication4/DefaultDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
 using Microsoft.Identity.Client;
 using System;
 using System.Collections.Generic;
@@ -23,7 +25,7 @@ namespace WebApplication4
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         { 
-
+           
 
         }
 
@@ -33,9 +35,31 @@ namespace WebApplication4
 
             modelBuilder.Entity<Employee>()
                 .HasOne(x=>x.Parent)
-                .WithMany(x=>x.Childrens)
+                .WithMany(x=>x.Children)
                 .HasForeignKey(x=>x.ParentId)
                 .OnDelete(DeleteBehavior.Restrict);
+        }
+
+
+        public async Task<List<Employee>> GetChilds(string Id)
+        {
+
+            var employee =await Employees.FromSqlRaw(
+                @"WITH Organization (id, name, parentId, below) AS 
+                    (
+                        SELECT id, name, ParentId, 0
+                        FROM dbo.Employees    
+                        WHERE Employees.Id = {0}         
+                        UNION ALL
+                        SELECT e.Id, e.name,e.ParentId, o.below + 1
+                        FROM dbo.Employees e    
+                        INNER JOIN organization o 
+                        ON o.Id = e.ParentId
+                    ) SELECT * FROM organization", Id)                 
+                .AsNoTrackingWithIdentityResolution()
+                .ToListAsync();
+
+            return employee;
         }
     }
 }
